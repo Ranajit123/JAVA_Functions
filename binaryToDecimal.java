bd146f9a2cc1ff13cbd3be09876cbf588fbde451
Ranajit Das <das143ranajit@gmail.com>
2023-08-13 08:48:07 +0530

    1st commit

---
+++ binaryToDecimal.java
@@ -0,0 +1,20 @@
+package Functions;
+
+public class binaryToDecimal {
+  public static void binToDeci(int n){
+    int pow =0;
+    int dec =0;
+    while(n>0){
+      int lastDigit = n%10;
+      dec= dec + (lastDigit*(int)Math.pow(2, pow));
+      pow ++;
+      n=n/10;
+    }
+    System.out.println(dec);
+  }
+
+  public static void main(String args[]){
+    binToDeci(101);
+  }
+  
+}
---
+++ decToBin.java
@@ -0,0 +1,16 @@
+package Functions;
+
+public class decToBin {
+  public static void dToB(int n){
+    while(n>0){
+      int binNum = n%2;
+      System.out.println(binNum);
+      n=n/2;
+    }
+
+  }
+  public static void main(String[] args){
+    dToB(7);
+  }
+  
+}
